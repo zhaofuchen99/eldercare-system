3388588be48ad4b279a47bfcd27273223ea7ba21
zhaofuchen <zhaofuchen22@gmail.com>
2026-08-16 16:40:28 +0800

    初始化Spring Boot项目

---
+++ src/main/java/com/zfc/eldercare/EldercareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zfc.eldercare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldercareSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EldercareSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zfc/eldercare/EldercareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfc.eldercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldercareSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
