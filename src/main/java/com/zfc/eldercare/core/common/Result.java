c558487cf190df8f293497a65e1ce156a59e98ba
zhaofuchen <zhaofuchen22@gmail.com>
2026-08-17 16:25:53 +0800

    同步接口文档

--- src/main/java/com/zfc/eldercare/core/common/Result.java
+++ src/main/java/com/zfc/eldercare/core/common/Result.java
@@ -3,6 +3,7 @@ package com.zfc.eldercare.core.common;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Setter
 public class Result<T> implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     /** 状态码 */
